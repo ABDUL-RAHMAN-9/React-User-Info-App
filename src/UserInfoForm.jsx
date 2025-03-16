56affccbd4a7b3686ce9eb1d83f0a5019422fe4c
Abdul Rahman <abdulrahman161004@gmail.com>
2025-03-16 23:37:06 +0530

    Created Dark Mode Toggle button

--- src/UserInfoForm.jsx
+++ src/UserInfoForm.jsx
@@ -1,5 +1,4 @@
-import React, { use } from 'react'
-import { useState } from 'react'    
+import { useState } from 'react'
 
 function UserInfoForm()
 {
@@ -7,13 +6,29 @@ function UserInfoForm()
     const [favoriteColor, setFavoriteColor] = useState("");
     const [age, setAge] = useState("");
     const [gretting, setGretting] = useState("");
-    const [darkMode,setDarkMode] = useState(false);
-    
-  return (
-    <div>
-      
-    </div>
-  )
+    const [darkMode, setDarkMode] = useState(false);
+
+
+    // styling for light & Dark Mode
+    const DarkModeStyling = {
+        backgroundColor: darkMode ? "#333" : "#fff",
+        color: darkMode ? "#fff" : "#333",
+    }
+    return (
+        <div>
+            <div className="flex flex-col min-h-screen items-center p-5 space-y-10" style={DarkModeStyling}>
+                <h1 className='text-5xl font-serif'>User Information Form</h1>
+                {/* Toggle for dark mode */}
+                <button
+                    onClick={() => { setDarkMode((prev) => !prev) }}
+                    className='border p-3 cursor-pointer'>
+                    Toggle Dark Mode
+                </button>
+            </div>
+
+
+        </div>
+    )
 }
 
 export default UserInfoForm
